d91da03ea498d5c088efc4aaa5437cc535def977
Parker Thomas <pthomas1997@gmail.com>
2015-12-17 23:07:31 -0500

    credentials

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = "E2KSc4kixgfo9xvLYjVMxiPWt"
+consumer_secret = "yQlSam4iRluzrZihFP8s0AUfp71LV5ZpkfrtfOKisCtjX4G7Gd"
+access_key = "4465616249-UL4EBnzWKhVxsjUg9R0YsYEAreqHGCrqwc5PuR0"
+access_secret = "VDKhdpC0frKY0iUVWh7PZidWGaCqSrl1dSSgV45aSyeoL"
